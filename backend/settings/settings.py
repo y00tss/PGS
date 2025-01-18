bbfde182101c0ccb63d2b807ed6e60a9296ea8ee
y00tss <shepel9119@gmail.com>
2025-01-18 12:57:20 +0300

    chore: static update
    - whitenoise was added
    - deleted static folder and collectstatic command was added

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 from settings.config import config as con
 import sys
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -134,8 +136,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+print("STATIC_ROOT", STATIC_ROOT)
 STATIC_URL = '/static/'
-STATIC_ROOT = "/static/"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
